75df40f86128ca9b54c2c6f6b79b346a3f92aedf
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-03-15 10:48:55 -0400

    Update Open This.cs

--- Capstone Game/Assets/Open This.cs
+++ Capstone Game/Assets/Open This.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class OpenThis : MonoBehaviour
 {
+    private GameObject a; // A gameobject is a object that appears in the game world
+    private Vector3 b; // A vector 3 is an x, y, and z that Unity uses to store position and velocity
+    private Vector2 c; // Is a vector 3 with one of the coordenates static
+    private Quaternion d; // A Quaternion is how Unity stores rotation and rotation speed
+    public int example; // A public variable can be edited inside the editor.
+    // If the game is running, no changes to these variables will save. It's good for testing
+
+
+
     // Start is called before the first frame update
     void Start()
     {
