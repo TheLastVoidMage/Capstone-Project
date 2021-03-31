34042e740d5a121594631a886c30aad9f0491c66
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-03-31 10:43:37 -0400

    Add interaction framework for player

--- Capstone Game/Assets/Scipts/Pickup.cs
+++ Capstone Game/Assets/Scipts/Pickup.cs
@@ -7,7 +7,7 @@ public class Pickup : MonoBehaviour
     // Tells what resource to add to, or if -1 then it's a gun
     public int resourceId = 0;
     public int quantity = 1;
-    //public GUN gun;
+    public Weapons weapon = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +29,10 @@ public class Pickup : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    //public int getGun()
-    //{
-    //    return gun;
-
-    //}
+    public Weapons getWeapon()
+    {
+        return weapon;
+    }
 
     // Update is called once per frame
     void Update()
