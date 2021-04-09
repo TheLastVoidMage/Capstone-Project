553a6937728a3bc4145e7ae806c37befcbd16d96
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-04-09 11:21:50 -0400

    Separated Starmap into Starmap and ShipController

--- Capstone Game/Assets/Scipts/Star Map/ShipController.cs
+++ Capstone Game/Assets/Scipts/Star Map/ShipController.cs
@@ -1,18 +1,172 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipController : MonoBehaviour
 {
+    private StarMap myMap;
+    private GameObject playerShip;
+    public Sprite playerShipSprite;
+    private Canvas bottomBar;
+    private Button travelButton;
+    private Text levelText;
+    public Sprite selectorSprite;
+    private RectTransform[] UIObjects;
+    private GameObject selector;
+    private float shipChance = 30;
+    private float[] timeLastMoved = { 0, 0 };
+    private int[] playerCoordinates = { 0, 0 };
+    private int[] selectorCoordinates = { 0, 0 };
+    private int fuel = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myMap = this.gameObject.GetComponent<StarMap>();
+        playerCoordinates[1] = Mathf.RoundToInt(myMap.levelHeight / 2);
+        selectorCoordinates[1] = playerCoordinates[1];
+        playerShip = new GameObject("Player Ship");
+        playerShip.transform.parent = this.transform;
+        playerShip.AddComponent<SpriteRenderer>().sprite = playerShipSprite;
+        playerShip.transform.localPosition = new Vector3((playerCoordinates[0] * myMap.levelSpacing) - 1, (playerCoordinates[1] * myMap.levelSpacing) + 1);
+        playerShip.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        bottomBar = GameObject.FindObjectOfType<Canvas>();
+        UIObjects = bottomBar.gameObject.transform.GetComponentsInChildren<RectTransform>();
+        for (int x = 0; x < UIObjects.Length; x++)
+        {
+            if (UIObjects[x].name == "TravelButton")
+            {
+                travelButton = UIObjects[x].GetComponent<Button>();
+            }
+            else if (UIObjects[x].name == "LevelInfo")
+            {
+                levelText = UIObjects[x].GetComponent<Text>();
+            }
+        }
+        selector = new GameObject();
+        selector.name = "Selector";
+        selector.transform.parent = this.transform;
+        selector.transform.localPosition = playerShip.transform.localPosition;
+        selector.AddComponent<SpriteRenderer>().sprite = selectorSprite;
+        selector.GetComponent<SpriteRenderer>().sortingOrder = -1;
+        Camera.main.transform.parent = selector.transform;
+        Camera.main.transform.localPosition = new Vector3(0, 0, -10);
+        timeLastMoved[0] = Time.time;
+        timeLastMoved[1] = Time.time;
+    }
+
+    private void handleTravelButton()
+    {
+        bool validButton = false;
+        string displayString = travelButton.GetComponentInChildren<Text>().text = "Travel\nCost: " + Mathf.Max(Mathf.Abs(selectorCoordinates[0] - playerCoordinates[0]), Mathf.Abs(selectorCoordinates[1] - playerCoordinates[1])) + " / " + fuel;
+        selector.transform.position = Vector3.Lerp(selector.transform.position, new Vector3(selectorCoordinates[0] * myMap.levelSpacing, selectorCoordinates[1] * myMap.levelSpacing), Time.deltaTime * 5);
+        if (playerShip.transform.localPosition != new Vector3((playerCoordinates[0] * myMap.levelSpacing) - 1, (playerCoordinates[1] * myMap.levelSpacing) + 1))
+        {
+            playerShip.transform.localPosition = Vector3.Lerp(playerShip.transform.localPosition, new Vector3((playerCoordinates[0] * myMap.levelSpacing) - 1, (playerCoordinates[1] * myMap.levelSpacing) + 1), Time.deltaTime * 5);
+        }
+        if (playerCoordinates[0] == selectorCoordinates[0] && playerCoordinates[1] == selectorCoordinates[1])
+        {
+            if (playerCoordinates[0] >= 0 && playerCoordinates[1] >= 0 && playerCoordinates[0] < myMap.levelWidth && playerCoordinates[1] < myMap.levelHeight)
+            {
+                if (myMap.ShipMap[playerCoordinates[0], playerCoordinates[1]] != null)
+                {
+                    if (myMap.ShipMap[playerCoordinates[0], playerCoordinates[1]].isVisited == false)
+                    {
+                        displayString = "Board";
+                        validButton = true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (fuel >= Mathf.Max(Mathf.Abs(selectorCoordinates[0] - playerCoordinates[0]), Mathf.Abs(selectorCoordinates[1] - playerCoordinates[1])))
+            {
+                validButton = true;
+            }
+
+        }
+        travelButton.GetComponentInChildren<Text>().text = displayString;
+        travelButton.interactable = validButton;
+    }
+
+    public void Travel()
+    {
+        if (playerCoordinates[0] == selectorCoordinates[0] && playerCoordinates[1] == selectorCoordinates[1])
+        {
+            if (playerCoordinates[0] >= 0 && playerCoordinates[1] >= 0 && playerCoordinates[0] < myMap.levelWidth && playerCoordinates[1] < myMap.levelHeight)
+            {
+                if (myMap.ShipMap[playerCoordinates[0], playerCoordinates[1]] != null)
+                {
+                    if (myMap.ShipMap[playerCoordinates[0], playerCoordinates[1]].isVisited == false)
+                    {
+                        myMap.ShipMap[playerCoordinates[0], playerCoordinates[1]].isVisited = true;
+                        myMap.ShipMap[playerCoordinates[0], playerCoordinates[1]].loadLevel();
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (fuel >= Mathf.Max(Mathf.Abs(selectorCoordinates[0] - playerCoordinates[0]), Mathf.Abs(selectorCoordinates[1] - playerCoordinates[1])))
+            {
+                fuel = fuel - Mathf.Max(Mathf.Abs(selectorCoordinates[0] - playerCoordinates[0]), Mathf.Abs(selectorCoordinates[1] - playerCoordinates[1]));
+                playerCoordinates[0] = selectorCoordinates[0];
+                playerCoordinates[1] = selectorCoordinates[1];
+            }
+        }
+        handleTravelButton();
+    }
+
+    private void manageControls()
+    {
+        if (Time.time - timeLastMoved[1] > .25f)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                selectorCoordinates[1] = selectorCoordinates[1] + 1;
+                timeLastMoved[1] = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                selectorCoordinates[1] = selectorCoordinates[1] - 1;
+                timeLastMoved[1] = Time.time;
+            }
+        }
+        if (Time.time - timeLastMoved[0] > .25f)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                selectorCoordinates[0] = selectorCoordinates[0] + 1;
+                timeLastMoved[0] = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                selectorCoordinates[0] = selectorCoordinates[0] - 1;
+                timeLastMoved[0] = Time.time;
+            }
+        }
+        handleTravelButton();
+        if (selectorCoordinates[0] < 0 || selectorCoordinates[1] < 0 || selectorCoordinates[0] >= myMap.levelWidth || selectorCoordinates[1] >= myMap.levelHeight)
+        {
+            levelText.text = "Dead Space Detected";
+        }
+        else
+        {
+            if (myMap.ShipMap[selectorCoordinates[0], selectorCoordinates[1]] != null)
+            {
+                levelText.text = "Ship Detected\nShip Visited: " + myMap.ShipMap[selectorCoordinates[0], selectorCoordinates[1]].isVisited + "\nSize: " + Level.valueNames[0, myMap.ShipMap[selectorCoordinates[0], selectorCoordinates[1]].levelValues[0]] + "\nLife sign Density: " + Level.valueNames[1, myMap.ShipMap[selectorCoordinates[0], selectorCoordinates[1]].levelValues[1]] + "\nLife Sign Strength: " + Level.valueNames[2, myMap.ShipMap[selectorCoordinates[0], selectorCoordinates[1]].levelValues[2]] + "\nLife Sign Type: " + Level.factionNames[myMap.ShipMap[selectorCoordinates[0], selectorCoordinates[1]].levelValues[3]];
+            }
+            else
+            {
+                levelText.text = "No Ship Found";
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        manageControls();
     }
 }
--- Capstone Game/Assets/Scipts/Star Map/StarMap.cs
+++ Capstone Game/Assets/Scipts/Star Map/StarMap.cs
@@ -2,192 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class StarMap : MonoBehaviour
 {
     public int levelWidth = 100;
     public int levelHeight = 25;
     public float levelSpacing = 5;
-    Level[,] ShipMap;
+    public Level[,] ShipMap;
     GameObject[,] ObjectMap;
-    private int playerX = 0;
-    private int playerY = 0;
-    private int selectedX = 0;
-    private int selectedY = 0;
-    private int fuel = 20;
-    private GameObject playerShip;
-    public Sprite playerShupSprite;
     public Sprite[] derilects;
-    private RectTransform[] UIObjects;
-    private Canvas bottomBar;
-    private Button travelButton;
-    private Text levelText;
-    public Sprite selectorSprite;
-    private GameObject selector;
     private float shipChance = 30;
-    private float[] timeLastMoved = { 0, 0 };
 
     // Start is called before the first frame update
     void Start()
     {
         ShipMap = new Level[levelWidth, levelHeight];
         ObjectMap = new GameObject[levelWidth, levelHeight];
-        playerY = Mathf.RoundToInt(levelHeight / 2);
-        selectedY = playerY;
         generateShipMap();
-        playerShip = new GameObject("Player Ship");
-        playerShip.transform.parent = this.transform;
-        playerShip.AddComponent<SpriteRenderer>().sprite = playerShupSprite;
-        playerShip.transform.localPosition = new Vector3((playerX * levelSpacing) - 1, (playerY * levelSpacing) + 1);
-        playerShip.GetComponent<SpriteRenderer>().sortingOrder = 1;
-        bottomBar = GameObject.FindObjectOfType<Canvas>();
-        UIObjects = bottomBar.gameObject.transform.GetComponentsInChildren<RectTransform>();
-        for (int x = 0; x < UIObjects.Length; x++)
-        {
-            if (UIObjects[x].name == "TravelButton")
-            {
-                travelButton = UIObjects[x].GetComponent<Button>();
-            }
-            else if (UIObjects[x].name == "LevelInfo")
-            {
-                levelText = UIObjects[x].GetComponent<Text>();
-            }
-        }
-        selector = new GameObject();
-        selector.name = "Selector";
-        selector.transform.parent = this.transform;
-        selector.transform.localPosition = playerShip.transform.localPosition;
-        selector.AddComponent<SpriteRenderer>().sprite = selectorSprite;
-        selector.GetComponent<SpriteRenderer>().sortingOrder = -1;
-        Camera.main.transform.parent = selector.transform;
-        Camera.main.transform.localPosition = new Vector3(0, 0, -10);
-        timeLastMoved[0] = Time.time;
-        timeLastMoved[1] = Time.time;
     }
 
-    private void handleTravelButton()
-    {
-        bool validButton = false;
-        string displayString = travelButton.GetComponentInChildren<Text>().text = "Travel\nCost: " + Mathf.Max(Mathf.Abs(selectedX - playerX), Mathf.Abs(selectedY - playerY)) + " / " + fuel;
-        selector.transform.position = Vector3.Lerp(selector.transform.position, new Vector3(selectedX * levelSpacing, selectedY * levelSpacing), Time.deltaTime * 5);
-        if (playerShip.transform.localPosition != new Vector3((playerX * levelSpacing) - 1, (playerY * levelSpacing) + 1))
-        {
-            playerShip.transform.localPosition = Vector3.Lerp(playerShip.transform.localPosition, new Vector3((playerX * levelSpacing) - 1, (playerY * levelSpacing) + 1), Time.deltaTime * 5);
-        }
-        if (playerX == selectedX && playerY == selectedY)
-        {
-            if (playerX >= 0 && playerY >= 0 && playerX < levelWidth && playerY < levelHeight)
-            {
-                if (ShipMap[playerX, playerY] != null)
-                {
-                    if (ShipMap[playerX, playerY].isVisited == false)
-                    {
-                        displayString = "Board";
-                        validButton = true;
-                    }
-                }
-            }
-        }
-        else
-        {
-            if (fuel >= Mathf.Max(Mathf.Abs(selectedX - playerX), Mathf.Abs(selectedY - playerY)))
-            {
-                validButton = true;
-            }
-
-        }
-        travelButton.GetComponentInChildren<Text>().text = displayString;
-        travelButton.interactable = validButton;
-    }
-
-    public void Travel()
-    {
-        if (playerX == selectedX && playerY == selectedY)
-        {
-            if (playerX >= 0 && playerY >= 0 && playerX < levelWidth && playerY < levelHeight)
-            {
-                if (ShipMap[playerX, playerY] != null)
-                {
-                    if (ShipMap[playerX,playerY].isVisited == false)
-                    {
-                        ShipMap[playerX, playerY].isVisited = true;
-                        ShipMap[playerX, playerY].loadLevel();
-                    }
-                }
-            }
-        }
-        else
-        {
-            if (fuel >= Mathf.Max(Mathf.Abs(selectedX - playerX), Mathf.Abs(selectedY - playerY)))
-            {
-                fuel = fuel - Mathf.Max(Mathf.Abs(selectedX - playerX), Mathf.Abs(selectedY - playerY));
-                playerX = selectedX;
-                playerY = selectedY;
-            }
-        }
-        handleTravelButton();
-    }
-
-    private void manageControls()
-    {
-        if (Time.time - timeLastMoved[1] > .25f)
-        {
-            if (Input.GetKey(KeyCode.W))
-            {
-                selectedY = selectedY + 1;
-                timeLastMoved[1] = Time.time;
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                selectedY = selectedY - 1;
-                timeLastMoved[1] = Time.time;
-            }
-        }
-        if (Time.time - timeLastMoved[0] > .25f)
-        {
-            if (Input.GetKey(KeyCode.D))
-            {
-                selectedX = selectedX + 1;
-                timeLastMoved[0] = Time.time;
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                selectedX = selectedX - 1;
-                timeLastMoved[0] = Time.time;
-            }
-        }
-        handleTravelButton();
-        if (selectedX < 0 || selectedY < 0 || selectedX >= levelWidth || selectedY >= levelHeight)
-        {
-            levelText.text = "Dead Space Detected";
-        }
-        else
-        {
-            if (ShipMap[selectedX, selectedY] != null)
-            {
-                levelText.text = "Ship Detected\nShip Visited: " + ShipMap[selectedX, selectedY].isVisited + "\nSize: " + Level.valueNames[0, ShipMap[selectedX, selectedY].levelValues[0]] + "\nLife sign Density: " + Level.valueNames[1, ShipMap[selectedX, selectedY].levelValues[1]] + "\nLife Sign Strength: " + Level.valueNames[2, ShipMap[selectedX, selectedY].levelValues[2]] + "\nLife Sign Type: " + Level.factionNames[ShipMap[selectedX, selectedY].levelValues[3]];
-            }
-            else
-            {
-                levelText.text = "No Ship Found";
-            }
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        manageControls();
-    }
-    
     private void generateShipMap()
     {
         for (int x = 0; x < ShipMap.GetLength(0); x++)
         {
             for (int y = 0; y < ShipMap.GetLength(1); y++)
             {
-                if (x != playerX || y != playerY)
+                if (x != 0 || y != Mathf.RoundToInt(levelHeight / 2))
                 {
                     if (Random.Range(0, 100) < shipChance)
                     {
